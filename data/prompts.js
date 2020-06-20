f51930257c060a11438efc42fcc4571f676ff767
aisyah-t <aisyah.n.tajudin@gmail.com>
2020-06-20 21:32:24 +1200

    Add routes and components

---
+++ data/prompts.js
@@ -0,0 +1,62 @@
+const prompts = [
+    {
+        id: 1,
+        prompt: "Write about someone who did something nice for you and how it made you feel."
+    },
+
+    {
+        id: 2,
+        prompt: "List three things you are grateful for right now."
+
+    },
+
+    {
+        id: 3,
+        prompt: "Write a thank you note to yourself."
+
+    },
+
+    {
+        id: 4,
+        prompt: "Write about something that makes you smile."
+
+    },
+
+    {
+        id: 5,
+        prompt: "What skills or abilities are you thankful to have?"
+
+    },
+
+    {
+        id: 6,
+        prompt: "What foods or meals are you most thankful for?"
+
+    },
+
+    {
+        id: 7,
+        prompt: "Describe a memory, and why it makes you feel grateful."
+
+    },
+
+    {
+        id: 8,
+        prompt: "Write about something you’re grateful to have learned this week."
+
+    },
+
+
+    {
+        id: 9,
+        prompt: "Write about the last time you laughed so hard you cried."
+
+    },
+
+    {
+        id: 10,
+        prompt: "What elements of nature are you grateful for and why?"
+
+    },
+    
+]
